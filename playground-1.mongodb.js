4a5530f454f43490c9864831c863a89a557c34c4
flynnm16 <flynnm16@udayton.edu>
2025-02-10 12:59:22 -0500

    summary of research for databases

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,43 +1,51 @@
-/* global use, db */
-// MongoDB Playground
-// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
-// Make sure you are connected to enable completions and to be able to run a playground.
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-// The result of the last command run in a playground is shown on the results panel.
-// By default the first 20 documents will be returned with a cursor.
-// Use 'console.log()' to print to the debug output.
-// For more documentation on playgrounds please refer to
-// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
-
 // Select the database to use.
 use('mongodbVSCodePlaygroundDB');
 
-// Insert a few documents into the sales collection.
+// Insert hierarchical merchandise data into the sales collection.
 db.getCollection('sales').insertMany([
-  { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-  { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
+  { 'category': 'Apparel', 'subCategory': 'T-shirt', 'price': 15, 'date': new Date('2024-01-10T10:00:00Z') },
+  { 'category': 'Apparel', 'subCategory': 'Hoodie', 'price': 40, 'date': new Date('2024-01-11T11:30:00Z') },
+  { 'category': 'Apparel', 'subCategory': 'Hat', 'price': 60, 'date': new Date('2024-01-12T12:45:00Z') },
+
+  { 'category': 'Beverage', 'subCategory': 'Water', 'price': 2, 'date': new Date('2024-02-01T08:00:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Soda Products', 'item': 'Coke', 'price': 5, 'date': new Date('2024-02-02T09:15:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Soda Products', 'item': 'Root Beer', 'price': 5, 'date': new Date('2024-02-02T09:15:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Soda Products', 'item': 'Sprite', 'price': 5, 'date': new Date('2024-02-02T09:15:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Soda Products', 'item': 'Orange', 'price': 5, 'date': new Date('2024-02-02T09:15:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Soda Products', 'item': 'Diet Coke', 'price': 5, 'date': new Date('2024-02-02T09:15:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Juice/Mocktails', 'item': 'Sparkling Water', 'price': 3, 'date': new Date('2024-02-03T10:30:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Juice/Mocktails', 'item': 'Lemonade', 'price': 3, 'date': new Date('2024-02-03T10:30:00Z') },
+  { 'category': 'Beverage', 'subCategory': 'Juice/Mocktails', 'item': 'Apple Juice', 'price': 3, 'date': new Date('2024-02-03T10:30:00Z') },
+
+ 
+  { 'category': 'Food', 'subCategory': 'Starters', 'item': 'Mozzarella Sticks', 'price': 1.5, 'date': new Date('2024-03-05T14:20:00Z') },
+  { 'category': 'Food', 'subCategory': 'Starters', 'item': 'Fries', 'price': 1.5, 'date': new Date('2024-03-05T14:20:00Z') },
+  { 'category': 'Food', 'subCategory': 'Starters', 'item': 'Tots', 'price': 1.5, 'date': new Date('2024-03-05T14:20:00Z') },
+  { 'category': 'Food', 'subCategory': 'Starters', 'item': 'Loaded Fries', 'price': 1.5, 'date': new Date('2024-03-05T14:20:00Z') },
+  { 'category': 'Food', 'subCategory': 'Starters', 'item': 'Loaded Tots', 'price': 1.5, 'date': new Date('2024-03-05T14:20:00Z') },
+  { 'category': 'Food', 'subCategory': 'Starters', 'item': 'Fried Pickles', 'price': 1.5, 'date': new Date('2024-03-05T14:20:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'Chicken Snack Wrap', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'Shrimp Tacos', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'Big Burger', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'Chicken Tender Sub', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'BLT', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'Salad', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Entrees', 'item': 'Chicken Tender Combo', 'price': 0.5, 'date': new Date('2024-03-06T15:45:00Z') },
+  { 'category': 'Food', 'subCategory': 'Wings', 'item': '6 for $10', 'price': 10, 'date': new Date('2024-03-07T16:10:00Z') },
+  { 'category': 'Food', 'subCategory': 'Wings', 'item': '8 for $12', 'price': 12, 'date': new Date('2024-03-07T16:10:00Z') },
+  { 'category': 'Food', 'subCategory': 'Wings', 'item': '12 for $16', 'price': 16, 'date': new Date('2024-03-07T16:10:00Z') }
 ]);
 
-// Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('sales').find({
-  date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
+// Find all sales within February 2024.
+const salesInFebruary = db.getCollection('sales').find({
+  date: { $gte: new Date('2024-02-01'), $lt: new Date('2024-03-01') }
 }).count();
 
 // Print a message to the output window.
-console.log(`${salesOnApril4th} sales occurred in 2014.`);
+console.log(`${salesInFebruary} sales occurred in February 2024.`);
 
-// Here we run an aggregation and open a cursor to the results.
-// Use '.toArray()' to exhaust the cursor to return the whole result set.
-// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+// Aggregation to find total sales per category.
 db.getCollection('sales').aggregate([
-  // Find all of the sales that occurred in 2014.
-  { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
-  // Group the total sales for each product.
-  { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
+  // Group by category and calculate total sales amount
+  { $group: { _id: '$category', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
 ]);
